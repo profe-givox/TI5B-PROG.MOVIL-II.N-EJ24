2c8cf688396d9c56fbb4ea54c3f66de6b80b2fef
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2024-01-26 15:24:48 -0600

    Commit inicial Avance: Se creo el metodo run y se mando llamar Tema: Corrutinas de Android Studio Laboratorio: Introducción a las corrutinas en Android Studio url: https://developer.android.com/codelabs/basic-android-kotlin-compose-coroutines-android-studio?hl=es-419&continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-compose-unit-5-pathway-1%3Fhl%3Des-419%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-compose-coroutines-android-studio&authuser=0#1

--- basic-android-kotlin-compose-training-race-tracker-starter/app/src/main/java/com/example/racetracker/ui/RaceParticipant.kt
+++ basic-android-kotlin-compose-training-race-tracker-starter/app/src/main/java/com/example/racetracker/ui/RaceParticipant.kt
@@ -18,6 +18,7 @@ package com.example.racetracker.ui
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import kotlinx.coroutines.delay
 
 /**
  * This class represents a state holder for race participant.
@@ -39,6 +40,13 @@ class RaceParticipant(
      */
     var currentProgress by mutableStateOf(initialProgress)
         private set
+
+    suspend fun run() {
+        while (currentProgress < maxProgress) {
+            delay(progressDelayMillis)
+            currentProgress += progressIncrement
+        }
+    }
 
     /**
      * Regardless of the value of [initialProgress] the reset function will reset the
