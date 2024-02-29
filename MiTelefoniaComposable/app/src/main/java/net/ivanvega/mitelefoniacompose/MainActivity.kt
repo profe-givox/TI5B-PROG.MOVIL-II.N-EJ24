06420d634813e4b77d47809bdfc43b6c35cc8d1a
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2024-02-29 13:32:30 -0600

    Se agrego un BroadcastReceiver a nivel de un elemento composable

--- MiTelefoniaComposable/app/src/main/java/net/ivanvega/mitelefoniacompose/MainActivity.kt
+++ MiTelefoniaComposable/app/src/main/java/net/ivanvega/mitelefoniacompose/MainActivity.kt
@@ -1,6 +1,13 @@
 package net.ivanvega.mitelefoniacompose
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.provider.Telephony
+import android.telephony.SmsMessage
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,7 +15,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import net.ivanvega.mitelefoniacompose.ui.theme.MiTelefoniaComposeTheme
 
@@ -22,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomeScreen()
                 }
             }
         }
@@ -35,6 +46,60 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+@Composable
+fun SystemBroadcastReceiver(
+    systemAction: String,
+    onSystemEvent: (intent: Intent?) -> Unit
+) {
+    // Grab the current context in this part of the UI tree
+    val context = LocalContext.current
+
+    // Safely use the latest onSystemEvent lambda passed to the function
+    val currentOnSystemEvent by rememberUpdatedState(onSystemEvent)
+
+    // If either context or systemAction changes, unregister and register again
+    DisposableEffect(context, systemAction) {
+        val intentFilter = IntentFilter(systemAction)
+        val broadcast = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                currentOnSystemEvent(intent)
+            }
+        }
+
+        context.registerReceiver(broadcast, intentFilter)
+
+        // When the effect leaves the Composition, remove the callback
+        onDispose {
+            context.unregisterReceiver(broadcast)
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
+
+
+
+    SystemBroadcastReceiver(Telephony.Sms.Intents.SMS_RECEIVED_ACTION) { intent ->
+        val isCharging = /* Get from batteryStatus ... */ true
+        /* Do something if the device is charging */
+        var strMensaje = ""
+        val bndSMS: Bundle? = intent?.getExtras()
+        val pdus = bndSMS?.get("pdus") as Array<Any>?
+        val smms: Array<SmsMessage?> = arrayOfNulls<SmsMessage>(pdus!!.size)
+        for (i in smms.indices) {
+            smms[i] = SmsMessage.createFromPdu(pdus!![i] as ByteArray)
+            strMensaje +="${"Mensaje: " + smms[i]?.getOriginatingAddress()}\n" +
+                    "${smms[i]?.getMessageBody().toString()}"
+
+
+        }
+        Log.d("MiBroadcastEnEjecucion", strMensaje)
+
+    }
+    Greeting(name = "Hola Mundo")
+    /* Rest of the HomeScreen */
 }
 
 @Preview(showBackground = true)
