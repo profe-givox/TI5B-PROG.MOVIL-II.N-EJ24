dc4e728ba3645e02e28a496503d64814526106fd
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2024-04-22 13:11:52 -0600

    Se agregaron las polilineas al Mapa

--- MiLocalizacionYMapasB/app/src/main/java/net/ivanvega/milocalizacionymapasb/ui/mapas/MapasScreen.kt
+++ MiLocalizacionYMapasB/app/src/main/java/net/ivanvega/milocalizacionymapasb/ui/mapas/MapasScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
@@ -17,18 +18,23 @@ import com.google.maps.android.compose.MapType
 import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.Polyline
 import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
 fun MiPrimerMapa() {
-    val singapore = LatLng(19.42709, -99.16765)
+    val singapore = LatLng(20.14025, -101.15112)
+
+    val polyline = listOf(   LatLng(20.14025, -101.15112),  LatLng(20.14338, -101.14988),
+        LatLng(20.14389, -101.15111))
+
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(singapore, 10f)
     }
 
     var uiSettings by remember { mutableStateOf(MapUiSettings()) }
     var properties by remember {
-        mutableStateOf(MapProperties(mapType = MapType.SATELLITE))
+        mutableStateOf(MapProperties(mapType = MapType.NORMAL))
     }
 
     Box(Modifier.fillMaxSize()) {
@@ -39,6 +45,7 @@ fun MiPrimerMapa() {
             uiSettings = uiSettings,
             cameraPositionState = cameraPositionState
         ) {
+            Polyline(points = polyline, color = Color.Red)
             Marker(
                 state = MarkerState(position = singapore),
                 title = "Mexico City",
